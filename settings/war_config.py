86f76a4f85ec70ebde36e88be6bc87c2f39525a3
SiLeNt.HeLL <Silent.Hell@smz.im>
2013-05-20 02:44:59 +0300

    added config for war CMD

---
+++ settings/war_config.py
@@ -0,0 +1,20 @@
+#   WAR Config file
+# -*- coding: utf-8 -*-
+#   zOeCher BOt v6.x
+{
+## Part 1 ##
+'Jid(s)-1': 'zoecher-'+str(random.randrange(00, 100)),
+'Server(s)-1': ['xmpp.ru','jabber.ru']
+'Password-1': '****',
+'Nick(s)-1': 'zOeCher'+str(random.randrange(00, 100)),
+## Part 2 ##
+'Jid(s)-2': '',
+'Server(s)-1': ['jwchat.org','jabber.ru','talk.mipt.ru','jabber.pm','jabber.re','jabber.mit.ru']
+'Password-2': '****',
+'Nick(s)-2': 'SM'+str(random.randrange(00, 100)),
+# --------------
+'Resource': 'zOeCher',
+'Status': 'zOeCher!',
+'Ignored_conf': 'santa@conference.smz.im',
+'Ignored_jid': 'silent.hell@smz.im'
+}
